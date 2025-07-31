a06ae3740230db2d14818c0f07ed14a8ec379b6e
amaljose-aj <57029243+amaljose-aj@users.noreply.github.com>
2025-07-31 10:11:13 +0530

    Update Checker.cpp
    Third commit

--- Checker.cpp
+++ Checker.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <cassert>
+#include <string>
 using namespace std;
 
-bool isTemperatureOk(float temperature) {
-  return (temperature >= 0 && temperature <= 45);
-}
+struct BatteryParam {
+  string name;
+  float value;
+  float min;
+  float max;
+};
 
-bool isSocOk(float soc) {
-  return (soc >= 20 && soc <= 80);
-}
-
-bool isChargeRateOk(float chargeRate) {
-  return (chargeRate <= 0.8);
+bool isInRange(float value, float min, float max) {
+  return value >= min && value <= max;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isTemperatureOk(temperature) &&
-         isSocOk(soc) &&
-         isChargeRateOk(chargeRate);
+  return isInRange(temperature, 0, 45) &&
+         isInRange(soc, 20, 80) &&
+         isInRange(chargeRate, 0, 0.8);
 }
 
 void logBatteryStatus(float temperature, float soc, float chargeRate) {
-  if (!isTemperatureOk(temperature)) {
-    cout << "Temperature out of range!\n";
-  }
-  if (!isSocOk(soc)) {
-    cout << "State of Charge out of range!\n";
-  }
-  if (!isChargeRateOk(chargeRate)) {
-    cout << "Charge Rate out of range!\n";
+  BatteryParam params[] = {
+    {"Temperature", temperature, 0, 45},
+    {"State of Charge", soc, 20, 80},
+    {"Charge Rate", chargeRate, 0, 0.8}
+  };
+
+  for (const auto& param : params) {
+    if (!isInRange(param.value, param.min, param.max)) {
+      cout << param.name << " out of range!\n";
+    }
   }
 }
 
@@ -36,5 +38,5 @@ int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
 
-  logBatteryStatus(50, 85, 0);
+  logBatteryStatus(50, 85, 0); // Will print out-of-range messages
 }
